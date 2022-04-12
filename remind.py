21b7cddd66cf2439cca20df95d9864833626fd36
colehearld <98860927+colehearld@users.noreply.github.com>
2022-04-11 20:04:22 -0400

    Update remind.py

--- remind.py
+++ remind.py
@@ -3,20 +3,45 @@ import schedule
 import time
 import config
 
-SID = config.account_sid
-token = config.auth_token
 
-client = Client(SID, token)
+client = Client(config.account_sid, config.auth_token)
 
-def send(t):
+userName = input("Please enter your name here: ")
+morningMeds = input("Please input your morning medications: ")
+eveningMeds = input("Please input your evening medications: ")
+
+helloMSG = "Hi "
+morningMSG = "your morning meds are "
+space = " "
+nightMSG = "your evening meds are "
+
+morningAnnouncement = helloMSG + userName + space + morningMSG + morningMeds
+eveningAnnouncement = helloMSG + userName + space + nightMSG + eveningMeds
+
+
+def send(x):
 
     call = client.messages.create(
-        config.to,
-        from_= "+18607184148",
-        body="Hi Madi! Don't forget your medications tonight."
+        to=config.to,
+        from_="+18607184148",
+        body=morningAnnouncement
     )
 
-schedule.every().day.at("22:00").do(send,'It is 22:00')
+
+schedule.every().day.at("08:00").do(send,'It is 08:00')
+
+
+def send(y):
+
+    call = client.messages.create(
+        to="+17047737799",
+        from_="+18607184148",
+        body=eveningAnnouncement
+    )
+
+
+schedule.every().day.at("21:22").do(send,'It is 21:22')
+
 
 while True:
     schedule.run_pending()
